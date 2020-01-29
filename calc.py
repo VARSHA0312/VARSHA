a16251eb77215310a2202f874b25b4e290b820ae
VARSHA0312 <60418668+VARSHA0312@users.noreply.github.com>
2020-01-29 10:56:37 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    sum=a+b
+    print(sum)
+def sub(a=10,b=6):
+    diff=a-b
+    print(diff)
+def mul(a=8,b=5):
+    ml=a*b 
+    print(ml)
+def div(b=100,a=10):
+    dv=a/b
+    print(dv)
+a=10
+b=20
+add(a,b)
+sub()
+mul(a,b)
+div()
